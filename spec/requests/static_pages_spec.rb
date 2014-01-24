d4bfad2ae1da20b097c87a445b4039261ba82f1b
Muhammad Anwar <anwar.anwarnor1990@gmail.com>
2014-01-24 10:54:12 +0800

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,28 +15,30 @@ describe "Static pages" do
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
 
-     describe "with 1 micropost" do
-        it { should have_content('1 micropost') }
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
       end
 
-      describe "with >1 microposts" do
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
         before do
-          FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+          other_user.follow!(user)
           visit root_path
         end
 
-        it "should render the user's feed" do
-          user.feed.each do |item|
-            page.should have_selector("li##{item.id}", text: item.content)
-          end
-        end
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
+  
 
     before { visit root_path }
     let(:heading)    { 'Sample App' }
